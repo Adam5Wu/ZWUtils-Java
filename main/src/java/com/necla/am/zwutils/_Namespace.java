d23ac546d3cbc877f67533a8746770dc7b652395
Zhenyu Wu <adamwu@nec-labs.com>
2016-06-06 20:28:56 -0400

    Improve namespace programmatic access in Eclipse debug mode

--- main/src/java/com/necla/am/zwutils/_Namespace.java
+++ main/src/java/com/necla/am/zwutils/_Namespace.java
@@ -9,8 +9,8 @@ public class _Namespace {
 	
 	public static final String PRODUCT = "ZWUtils - Jack of All Trades Utility Libraries";
 	public static final String COMPONENT = "ZWUtils for Java";
-	public static final String VERSION = "@VERSION@";
-	public static final String BUILD = "@DATE@ @TIME@";
+	public static final String VERSION = "@VERSION@".toString();
+	public static final String BUILD = "@DATE@ @TIME@".toString();
 	
 	static {
 		Versioning.Namespace_Init(_Namespace.class);
