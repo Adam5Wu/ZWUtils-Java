f47f5963f62ec182c2ca7217e93cce0551701868
Zhenyu Wu <adamwu@nec-labs.com>
2018-01-31 20:33:16 -0500

    Add more test cases

--- test/src/java/junit/com/necla/am/zwutils/junit/TestMisc.java
+++ test/src/java/junit/com/necla/am/zwutils/junit/TestMisc.java
@@ -25,4 +25,79 @@ public class TestMisc {
 		}
 	}
 	
+	@Test
+	public void CustomThrowWithFAIL_UsingFormattedMessage() {
+		final String TestFormatMsg = "FAIL() from '%s'";
+		try {
+			Misc.FAIL(TestFormatMsg, this.getClass());
+		} catch (RuntimeException e) {
+			Assert.assertEquals(String.format(TestFormatMsg, this.getClass()), e.getMessage());
+		}
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void CustomThrowWithFAILUsingSpecificException() {
+		Misc.FAIL(IllegalStateException.class, "Just a test");
+	}
+	
+	@Test(expected = Error.class)
+	public void CustomThrowWithERROR() {
+		Misc.ERROR();
+	}
+	
+	@Test
+	public void CustomThrowWithERROR_HasCorrectStackFrame() {
+		try {
+			Misc.ERROR();
+		} catch (Error e) {
+			StackTraceElement[] Stack = Misc.sliceStackTrace(e.getStackTrace(), 0, 1);
+			Assert.assertEquals(this.getClass().getName(), Stack[0].getClassName());
+			Assert.assertEquals("CustomThrowWithERROR_HasCorrectStackFrame", Stack[0].getMethodName());
+		}
+	}
+	
+	@Test
+	public void CustomThrowWithERRORL_UsingFormattedMessage() {
+		final String TestFormatMsg = "ERROR() from '%s'";
+		try {
+			Misc.ERROR(TestFormatMsg, this.getClass());
+		} catch (Error e) {
+			Assert.assertEquals(String.format(TestFormatMsg, this.getClass()), e.getMessage());
+		}
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void CascadeFAIL() {
+		try {
+			Misc.FAIL("Test Cascade FAIL()");
+		} catch (RuntimeException e) {
+			Misc.CascadeThrow(e);
+		}
+	}
+	
+	@Test(expected = Error.class)
+	public void CascadeERROR() {
+		try {
+			Misc.ERROR("Test Cascade ERROR()");
+		} catch (Error e) {
+			Misc.CascadeThrow(e);
+		}
+	}
+	
+	@Test
+	public void CascadeFAIL_HasCorrectCause() {
+		final String TestMsg = "Inner Exception";
+		try {
+			try {
+				Misc.FAIL(TestMsg);
+			} catch (RuntimeException e) {
+				Misc.CascadeThrow(e);
+			}
+		} catch (RuntimeException e) {
+			Throwable c = e.getCause();
+			Assert.assertNotNull(c);
+			Assert.assertEquals(TestMsg, c.getMessage());
+		}
+	}
+	
 }
